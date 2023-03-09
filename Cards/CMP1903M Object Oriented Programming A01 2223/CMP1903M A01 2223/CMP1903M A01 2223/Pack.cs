fe29cda90c1a442cd74e8236db3810dac7133fa3
deeknowy <127259414+deeknowy@users.noreply.github.com>
2023-03-09 09:05:39 +0000

    Update Pack.cs

--- Cards/CMP1903M Object Oriented Programming A01 2223/CMP1903M A01 2223/CMP1903M A01 2223/Pack.cs
+++ Cards/CMP1903M Object Oriented Programming A01 2223/CMP1903M A01 2223/CMP1903M A01 2223/Pack.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,26 +22,28 @@ namespace CMP1903M_A01_2223
             pack = new Card[numOfCards];
             currentCard = 0;
             randNum = new Random();
-            for (int i = 0; i < pack.Length - 1; i++)
+            for (int i = 0; i <= pack.Length - 1 ; i++)
                 pack[i] = new Card(Value[i % Value.Length], Suit[i / Value.Length]);
         }
 
         public bool shuffleCardPack(int typeOfShuffle)
-        {  
+        {
             //Shuffles the pack based on the type of shuffle
             randNum = new Random();
             switch (typeOfShuffle)
             {
                 //FisherYatesShuffle
                 case 0:
-                    for (int i = pack.Length - 1; i >= 1; i--)
                     {
-                        int j = randNum.Next(i + 1);
-                        Card temp = pack[j];
-                        pack[j] = pack[i];
-                        pack[i] = temp;
+                        for (int i = pack.Length - 1; i > 0; i--)
+                        {
+                            int j = randNum.Next(i + 1);
+                            Card temp = pack[i];
+                            pack[i] = pack[j];
+                            pack[j] = temp;
+                        }
+                        return true;
                     }
-                    return true;
                 //Riffle Shuffle
                 case 1:
                     for (int i = pack.Length - 1; i > 0; i--)
@@ -81,4 +83,3 @@ namespace CMP1903M_A01_2223
         }
     }
 }
- 
