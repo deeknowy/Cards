6b5b06759e064e171296b035eb3b615fdbd77ac9
deeknowy <127259414+deeknowy@users.noreply.github.com>
2023-03-09 04:15:49 +0000

    Add files via upload

---
+++ Cards/CMP1903M Object Oriented Programming A01 2223/CMP1903M A01 2223/CMP1903M A01 2223/Card.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace CMP1903M_A01_2223
+{
+    class Card
+    {
+        private string vNum;
+        private string sNum;
+
+        public Card(string AvNum, string AsNum)
+        {
+            sNum = AsNum;
+            vNum = AvNum;
+        }
+
+
+        //Base for the Card class.
+
+        //The 'set' methods for these properties could have some validation
+        //Value: numbers 1 - 13
+        public string Value
+        {
+            get { return vNum; }
+            set
+            {
+                if (value == "ACE" || value == "TWO" || value == "THREE" || value == "FOUR" || value == "FIVE" || value == "SIX" || value == "SEVEN" || value == "EIGHT" || value == "NINE" || value == "TEN" || value == "JACK" || value == "QUEEN" || value == "KING")
+                {
+                    vNum = value;
+                }
+            }
+        }
+        //Suit: numbers 1 - 4
+        public string Suit
+        { 
+            get { return sNum; } 
+            set
+            {
+                if (value == "HEARTS" || value == "CLUBS" || value == "DIAMONDS" || value == "SPADES")
+                {
+                    sNum = value;
+                }
+            }
+        }
+    }
+}
